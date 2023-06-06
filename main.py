843b35a822b8f28d85c8bcad954f0a4ca9e0be60
ausidop <ausidop@gmail.com>
2023-06-05 22:03:07 -0600

    add comments, replace top100 code

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
+# memory to track IP addresses and frequency in dictionary
+# IP address as key and visited number in value
+import heapq
+
 ipAddresses = dict()
 
-def requestHandled(ipAddress: str):
+
+# take IP address string as input and store into the memory
+# memory will take IP address as key and frequency of visits as value
+def requestHandled(ipAddress):
     ipAddresses[ipAddress] = ipAddresses.setdefault(ipAddress, 1) + 1
 
+
+# return top 100 ip address with the highest traffic
 def top100():
-    top100 = sorted(ipAddresses, key=lambda x: x[1], reverse=True)
-    if len(top100) >= 100:
-        return top100[:100]
+    top100 = heapq.nlargest(100, ipAddresses, key=ipAddresses.get)
     return top100
 
+
+# clear the memory
 def clear():
     stack = dict()
