74c34180f9ec67e6f686384f848958d0edaf4731
ausidop <ausidop@gmail.com>
2023-06-05 22:09:41 -0600

    wrap functions into class

--- main.py
+++ main.py
@@ -2,21 +2,20 @@
 # IP address as key and visited number in value
 import heapq
 
-ipAddresses = dict()
+class IP_Tracker:
+    def __init__(self):
+        self.ipAddresses = dict()
 
+    # take IP address string as input and store into the memory
+    # memory will take IP address as key and frequency of visits as value
+    def requestHandled(self, ipAddress):
+        self.ipAddresses[ipAddress] = self.ipAddresses.setdefault(ipAddress, 1) + 1
 
-# take IP address string as input and store into the memory
-# memory will take IP address as key and frequency of visits as value
-def requestHandled(ipAddress):
-    ipAddresses[ipAddress] = ipAddresses.setdefault(ipAddress, 1) + 1
+    # return top 100 ip address with the highest traffic
+    def top100(self):
+        top100 = heapq.nlargest(100, self.ipAddresses, key=self.ipAddresses.get)
+        return top100
 
-
-# return top 100 ip address with the highest traffic
-def top100():
-    top100 = heapq.nlargest(100, ipAddresses, key=ipAddresses.get)
-    return top100
-
-
-# clear the memory
-def clear():
-    stack = dict()
+    # clear the memory
+    def clear(self):
+        self.ipAddresses = dict()
